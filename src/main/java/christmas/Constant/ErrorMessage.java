a4898f5088171d234bac45efa5f3592040eba8b5
koomin1227 <koomin1227@naver.com>
2024-01-23 00:10:55 +0900

    feat: 유효하지 않은 주문 메시지 추가

--- src/main/java/christmas/Constant/ErrorMessage.java
+++ src/main/java/christmas/Constant/ErrorMessage.java
@@ -1,7 +1,9 @@
 package christmas.Constant;
 
 public enum ErrorMessage {
-    INVALID_DATE("유효하지 않은 날짜입니다.");
+    INVALID_DATE("유효하지 않은 날짜입니다."),
+    INVALID_ORDER("유효하지 않은 주문입니다.");
+
     private final String message;
     ErrorMessage(String message) {
         this.message = message;
