c9c7651d76780e03d879cb02f095ff78e4d36d71
koomin1227 <koomin1227@naver.com>
2024-01-22 21:48:37 +0900

    feat: INVALID_DATE 메시지 정의

---
+++ src/main/java/christmas/Constant/ErrorMessage.java
@@ -0,0 +1,13 @@
+package christmas.Constant;
+
+public enum ErrorMessage {
+    INVALID_DATE("유효하지 않은 날짜입니다.");
+    private final String message;
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
