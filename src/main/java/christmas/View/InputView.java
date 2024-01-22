2eb4c7191b7ff8196dcf9f1bdb27e17426d24b77
koomin1227 <koomin1227@naver.com>
2024-01-22 22:08:44 +0900

    fix: date를 검증 하는 부분을 Date 객체에게 넘기도록 수정

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,35 +1,19 @@
 package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.Constant.ErrorMessage;
+import christmas.Domain.Date;
 
 public class InputView {
-    private static final int START_DATE = 1;
-    private static final int END_DATE = 31;
-    public static int readDate() {
+    public static Date readDate() {
         while (true) {
             try{
                 System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
                 String input = Console.readLine();
-                validateDate(input);
-                return Integer.parseInt(input);
+                Date date = new Date(input);
+                return date;
             } catch(IllegalArgumentException e) {
                 System.out.println(e.getMessage() + " 다시 입력해 주세요.");
             }
         }
-    }
-
-    private static void validateDate(String input) {
-        if (!isNumeric(input)) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.getMessage());
-        }
-        int date = Integer.parseInt(input);
-        if (date < START_DATE || date > END_DATE) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.getMessage());
-        }
-    }
-
-    private static boolean isNumeric(String input) {
-        return input.matches("\\d+");
     }
 }
