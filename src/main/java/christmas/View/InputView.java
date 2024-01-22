9343d205475b3145c62c2f30a74bd3e708400007
koomin1227 <koomin1227@naver.com>
2024-01-23 01:03:44 +0900

    feat: 주문을 받는 메서드 구현

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -2,6 +2,11 @@ package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.Domain.Date;
+import christmas.Domain.Order;
+import christmas.Domain.OrderList;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     public static Date readDate() {
@@ -12,7 +17,20 @@ public class InputView {
                 Date date = new Date(input);
                 return date;
             } catch(IllegalArgumentException e) {
-                System.out.println(e.getMessage() + " 다시 입력해 주세요.");
+                System.out.println("[ERROR]" + e.getMessage() + " 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    public static OrderList readOrder() {
+        while (true) {
+            try{
+                System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+                String input = Console.readLine();
+                List<Order> orders = Arrays.stream(input.split(",")).map(Order::new).toList();
+                return new OrderList(orders);
+            } catch(IllegalArgumentException e) {
+                System.out.println("[ERROR]" + e.getMessage() + " 다시 입력해 주세요.");
             }
         }
     }
