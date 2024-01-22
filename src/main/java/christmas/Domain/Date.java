1a74b7fc32d846d6c0b9bb1a79028bc74b7f2949
koomin1227 <koomin1227@naver.com>
2024-01-22 23:07:24 +0900

    feat: 날짜의 종류를 반환하기능 추가

--- src/main/java/christmas/Domain/Date.java
+++ src/main/java/christmas/Domain/Date.java
@@ -1,14 +1,19 @@
 package christmas.Domain;
 
+import christmas.Constant.DateKind;
+import christmas.Constant.DateKindList;
 import christmas.Constant.ErrorMessage;
 
 public class Date {
     private static final int START_DATE = 1;
     private static final int END_DATE = 31;
     private int date;
+    private String kind;
+
     public Date(String date) {
         validateDate(date);
         this.date = Integer.parseInt(date);
+        setKind();
     }
 
     private void validateDate(String input) {
@@ -23,5 +28,30 @@ public class Date {
 
     private boolean isNumeric(String input) {
         return input.matches("\\d+");
+    }
+
+    private void setKind() {
+        if (isInList(DateKindList.STAR_DAY_LIST.getDayList())) {
+            this.kind = DateKind.STAR_DAY.getKind();
+            return;
+        }
+        if (isInList(DateKindList.WEEKEND_LIST.getDayList())) {
+            this.kind = DateKind.WEEKEND.getKind();
+            return;
+        }
+        this.kind = DateKind.WEEKDAY.getKind();
+    }
+
+    private boolean isInList(int [] list) {
+        for (int date : list) {
+            if (this.date == date) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getKind() {
+        return kind;
     }
 }
