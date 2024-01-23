56f1c7c9853a1f1a86b8bd15aa4a8e60a93602cd
koomin1227 <koomin1227@naver.com>
2024-01-23 19:28:09 +0900

    fix: 평일 날짜리스트 수정

--- src/main/java/christmas/Constant/DateKindList.java
+++ src/main/java/christmas/Constant/DateKindList.java
@@ -1,7 +1,7 @@
 package christmas.Constant;
 
 public enum DateKindList {
-    WEEKDAY_LIST(new int[]{4,5,6,7,11,12,13,14,18,19,20,21,26,27,28}),
+    WEEKDAY_LIST(new int[]{3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31}),
     WEEKEND_LIST(new int[]{1,2,8,9,15,16,22,23,29,30}),
     STAR_DAY_LIST(new int[]{3,10,17,24,25,31});
 
