45d51c05f16a9bb0793b4db46362fcc7a5fb5fff
koomin1227 <koomin1227@naver.com>
2024-01-23 21:28:21 +0900

    feat:  평일, 주말, 특별 할인 계산 구현

--- src/main/java/christmas/Domain/Event.java
+++ src/main/java/christmas/Domain/Event.java
@@ -1,12 +1,15 @@
 package christmas.Domain;
 
 import java.util.HashMap;
+import static christmas.Constant.DateKind.*;
+import static christmas.Constant.Category.*;
 
 public class Event {
     private static final int PRESENTATION_BOUND = 120000;
     private int totalBenefit = 0;
     private int totalDiscount = 0;
-    private HashMap<String, Integer> benefitList = new HashMap<>();
+
+    private final HashMap<String, Integer> benefitList = new HashMap<>();
     Event(Date date, OrderList orderList) {
         if (orderList.getTotalPrice() > 10000){
             setPresentation(orderList.getTotalPrice());
@@ -16,6 +19,9 @@ public class Event {
 
     private void setDiscount(Date date, OrderList orderList) {
         setChristmasDiscount(date);
+        setWeekdayDiscount(date, orderList);
+        setWeekendDiscount(date, orderList);
+        setStarDayDiscount(date);
     }
 
     private void setPresentation(int totalPrice) {
@@ -34,4 +40,52 @@ public class Event {
         }
     }
 
+    private void setWeekdayDiscount(Date date, OrderList orderList) {
+        if (date.getKind().contains(WEEKDAY.getKind())) {
+            int discountPrice = 0;
+            for ( Order order : orderList.getOrders() ) {
+                if (order.getMenu().getCategory().equals(DESSERT)) {
+                    discountPrice += 2043 * order.getCount();
+                }
+            }
+            totalBenefit += discountPrice;
+            totalDiscount += discountPrice;
+            benefitList.put("평일 할인", discountPrice);
+        }
+    }
+
+    private void setWeekendDiscount(Date date, OrderList orderList) {
+        if (date.getKind().contains(WEEKEND.getKind())) {
+            int discountPrice = 0;
+            for ( Order order : orderList.getOrders() ) {
+                if (order.getMenu().getCategory().equals(MAIN)) {
+                    discountPrice += 2043 * order.getCount();
+                }
+            }
+            totalBenefit += discountPrice;
+            totalDiscount += discountPrice;
+            benefitList.put("주말 할인", discountPrice);
+        }
+    }
+
+    private void setStarDayDiscount(Date date) {
+        if (date.getKind().contains(STAR_DAY.getKind())) {
+            int discountPrice = 1000;
+            totalBenefit += discountPrice;
+            totalDiscount += discountPrice;
+            benefitList.put("특별 할인", discountPrice);
+        }
+    }
+
+    public int getTotalBenefit() {
+        return totalBenefit;
+    }
+
+    public int getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public HashMap<String, Integer> getBenefitList() {
+        return benefitList;
+    }
 }
